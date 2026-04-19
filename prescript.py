f726480fc8476564de35dbca66b9f5db5f1cde50
SiKeaw <sirawichayakeawgerd@gmail.com>
2026-04-19 22:46:45 +0700

    Comment out Text_group variable in prescript.py

--- prescript.py
+++ prescript.py
@@ -17,19 +17,19 @@ def text_scrambler(text):
         time.sleep(1 / 3125)
     print()
 
-Text_group = [
-    'To ..., place your hands on the ground and pray.',
-    'To ..., jump over and over again until you are tired.',
-    'To ..., see green from a white wall.',
-    'To ..., see the world in a different way.',
-    'To ..., At work ,cut work the ear of the first person to fulminate aganist you.',
-    'To ..., Pet quadrupedal animals five times a day.'
-    'To ..., Do not eat anything for a day and a night.',
-    'To ..., Return to your home this instant. You may leave once a dog barks in front of your house one time.',
-    'Write a song for your eleventh neighbor down the road, then play tag with your eleventh neighbor down the road. You do not need to win. Time limit: 84 months.',
-    'To ..., watch a movie that you have never seen before, and then write a review about it.',
-    'To ..., write a poem about the beauty of nature, and then read it aloud in public.',
-    'To ..., Within three days, knit scarf with butterfly pattern, and then give it to a stranger.',
+#Text_group = [
+#    'Sentence',
+#    'Sentence',
+#    'Sentence',
+#    'Sentence',
+#    'Sentence',
+#    'Sentence'
+#    'Sentence',
+#    'Sentence',
+#    'Sentence',
+#    'Sentence',
+#    'Sentence',
+#    'Sentence',
 
 
 ]
